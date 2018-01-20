c2dccd6be5f604d7f3ec5e923f065479868c5274
Dean Herbert <pe@ppy.sh>
2018-01-20 15:40:34 +0900

    Add workaround for github api only returning one tag per commit hash at maximum

--- _scripts/create_changelog.sh
+++ _scripts/create_changelog.sh
@@ -30,15 +30,18 @@ do
 
     echo "Running for project $name"
     
-    last_tag=$(cd $repo_path; git fetch -t upstream; git tag | grep changelog | tail -1)
+    last_tag_local=$(cd $repo_path; git fetch -t upstream; git tag | grep changelog | tail -1)
+    last_tag_hash=$(cd $repo_path; git rev-list -n1 $last_tag_local)
+    last_tag=$(curl --silent https://api.github.com/repos/ppy/$name/tags | jsawk "if (this.commit.sha == \"$last_tag_hash\") return this.name; else return null" -a 'return this.join(" ")')
 
-    echo "Last tag: $last_tag"
+    echo "Last tag: $last_tag_hash (local: $last_tag_local github: $last_tag)"
 
-    $github_changelog_generator -o $name.md --since-tag "$last_tag" --exclude-tags-regex "^((?!changelog).)*$" --max-issues 0 --no-issues --no-compare-link --release-branch master --simple-list https://github.com/ppy/$name
-    gtail -n +4 $name.md | ghead -n -4 > $name_trimmed.md
+    $github_changelog_generator -o $name.md --since-tag $last_tag --max-issues 500 --no-issues --no-compare-link --release-branch master --simple-list https://github.com/ppy/$name
+    gtail -n +5 $name.md | ghead -n -4 | sed 's/^## /#### /' > $name_trimmed.md
     
     if [ -s $name_trimmed.md ]; then 
-        echo -e "\n## [$name](https://github.com/ppy/$name) *$description*\n\n### Important Changes\n\n### Other Changes" >> $output_file
+        echo -e "\n## [$name](https://github.com/ppy/$name) *$description*\n\n### Important Changes\n\n### Other Changes\n\n#### Unreleased changes" >> $output_file
+        
         cat $name_trimmed.md >> $output_file
 
         tag_name=$(date +'changelog-%Y%m%d')
